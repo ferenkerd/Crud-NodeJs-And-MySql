85ef48e8e43fb699c2731c0c8500a366c4c7c93d
ferenkerd <ferenkersanguinodh@gmail.com>
2023-11-19 13:44:15 -0400

    modified database connection, fixed

--- database/db.js
+++ database/db.js
@@ -1,7 +1,7 @@
-import mysql from "mysql"
+import mysql from "mysql2/promise"
 import {DB_HOST, DB_USER, DB_PASSWORD, DB_PORT, DB_NAME} from "../config.js"
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
     host: DB_HOST,
     user: DB_USER,
     password: DB_PASSWORD,
@@ -9,12 +9,26 @@ const connection = mysql.createConnection({
     database: DB_NAME
 })
 
-connection.connect((error) => {
+
+connection.getConnection((error, conn) => {
     if(error) {
         console.error("error en la conexion "+error)
-        return
+        return 
     }
     console.log("conectado a la base de datos mysql")
+    conn.release()
 })
+
+async function testConnection() {
+    try {
+        const conn = await connection.getConnection()
+        console.log("conectado a la base de datos mysql")
+        conn.release()
+    }catch (error) {
+        console.error("error en la conexion "+error)
+    }
+}
+
+testConnection()
 
 export default connection
